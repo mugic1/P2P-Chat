72578469ffd66281f402daf44c35a94296f52e3e
mugic1 <dasandrew047@gmail.com>
2026-05-17 12:02:57 +0530

    Update service worker cache name and event listeners

--- sw.js
+++ sw.js
@@ -1,27 +1,44 @@
-const CACHE = "peerdrop-pro-v1";
+const CACHE =
+"peerdrop-ultra-v1";
 
 const FILES = [
+
 "./",
 "./index.html",
 "./style.css",
 "./app.js",
 "./manifest.json"
+
 ];
 
-self.addEventListener("install", e=>{
+self.addEventListener(
+"install",
+e=>{
 
 e.waitUntil(
+
 caches.open(CACHE)
-.then(cache=>cache.addAll(FILES))
+.then(cache=>
+cache.addAll(FILES)
+)
+
 );
 
-});
+}
+);
 
-self.addEventListener("fetch", e=>{
+self.addEventListener(
+"fetch",
+e=>{
 
 e.respondWith(
+
 caches.match(e.request)
-.then(res=>res || fetch(e.request))
+.then(res=>
+res || fetch(e.request)
+)
+
 );
 
-});
+}
+);
